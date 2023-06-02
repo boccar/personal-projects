551343100cf7b0e8dbfb0eb5817a4f9a6edfac66
Henrique Bocca Corrêa <106291993+boccar@users.noreply.github.com>
2023-06-02 17:12:16 -0300

    Delete bd_connect.php

--- form-funcional/assets/config/bd_connect.php
+++
@@ -1,9 +0,0 @@
-<?
-    // Conectar ao banco de dados
-    $host = 'localhost';  // substitua pelo seu host
-    $db   = 'sciencca'; // substitua pelo nome do seu banco de dados
-    $user = 'sciencca'; // substitua pelo nome de usuário do banco de dados
-    $pass = 'hEGRjRsFPRnfeHtb'; // substitua pela senha do banco de dados
-
-    $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-?>
